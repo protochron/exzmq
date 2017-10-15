ab909ae146e069d126ed9cd5b60c07e04172549f
Dan Norris <protochron@gmail.com>
2017-10-14 20:02:25 -0700

    Fix credo and compiler warnings

--- lib/exzmq/address.ex
+++ lib/exzmq/address.ex
@@ -9,17 +9,15 @@ defmodule Exzmq.Address do
   # port: the port number
   defstruct transport: :tcp, ip: nil, port: nil
 
-  def parse(%Exzmq.Address{} = address) do
-    address
-  end
+  def parse(%Exzmq.Address{} = address), do: address
   def parse(address) do
     unless address |> String.starts_with?("tcp://") do
       raise "Unsupported transport: #{address}"
     end
     [ip, port] = address
-    |> String.slice(6,999)
+    |> String.slice(6, 999)
     |> String.split(":")
-    {:ok, ip} = ip |> String.to_char_list |> :inet.parse_address
+    {:ok, ip} = ip |> String.to_charlist |> :inet.parse_address
     %Exzmq.Address{ip: ip, port: port |> String.to_integer}
   end
 
